6b5d268a6fed958c21665ae292bc665c3ace0578
andres acosta <andresfelipeacostagarcia34@gmail.com>
2026-03-15 17:53:02 -0400

    fix: previous

--- app.py
+++ app.py
@@ -78,11 +78,11 @@ def clear_cart():
 
 @app.route('/ready')
 def ready():
-    return jsonify({"status": "ready"})
+    return "OK", 200
 
 @app.route('/healthz')
 def healthz():
-    return jsonify({"status": "healthy"})
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
